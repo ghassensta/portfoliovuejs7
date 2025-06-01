56ee8a1da9098eb17da71911e9f2187455aa88e9
ghassen <123700710+ghassensta@users.noreply.github.com>
2025-06-01 18:26:51 +0100

    m-version

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,17 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import svgLoader from 'vite-svg-loader';          // npm i -D vite-svg-loader
 import path from 'path';
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    svgLoader(),                                  //  <-- supprime si tu n’en as pas besoin
-  ],
+  plugins: [vue()],
 
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),        //  @  =  /src
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 
-  /* (facultatif) : taille d’alerte des chunks, etc. */
   build: {
     chunkSizeWarningLimit: 1000,
   },
